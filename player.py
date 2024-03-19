e370e6ba013b5a0f3039cf406915f281d970425b
Erik Diegel <1034@student.itgymnazium.cz>
2024-03-19 09:58:46 +0100

    All animations except for fall animation are working

--- player.py
+++ player.py
@@ -18,30 +18,37 @@ class Player:
         self.move_right = False
         self.move_up = False
         self.is_jumping = False
+        self.is_falling = False
         self.can_jump = True
         self.jump_force = jump_force
         self.gravity = gravity
         self.vertical_velocity = vertical_velocity
         self.rect = pygame.Rect(x, y, width, height)
+        self.facing_right = True
         self.assets = {
-            "player_idle": Animation(load_images("Idle_right"), img_dur=10),
-            "player_jump": Animation(load_images("Jump_right")),
-            # "player_run": Animation(load_images("Graphics/player/run"), img_dur=4)
+            "player_idle": Animation(load_images("Idle"), img_dur=10),
+            "player_jump": Animation(load_images("Jump")),
+            "player_run": Animation(load_images("run"), img_dur=4),
+            "player_fall": Animation(load_images("fall"))
         }
         self.current_animation = 'player_idle'
 
     def update_animation(self):
         if self.is_jumping:
-            self.current_animation = 'player_jump'
-        # elif self.move_left or self.move_right:
-        # self.current_animation = 'player_run'
+            self.current_animation = "player_jump"
+        elif self.move_left or self.move_right:
+            self.current_animation = "player_run"
+        elif self.is_falling:
+            self.current_animation = "player_fall"
         else:
-            self.current_animation = 'player_idle'
+            self.current_animation = "player_idle"
 
     def draw(self, camera):
         current_animation = self.assets[self.current_animation]  # Access the current animation
         current_animation.update()  # Update the animation frame
         current_frame = current_animation.img()  # Get the current frame
+        if not self.facing_right:
+            current_frame = pygame.transform.flip(current_frame, True, False)
 
         rect = camera.apply(self.rect)
         self.screen.blit(current_frame, rect.topleft)
@@ -50,8 +57,10 @@ class Player:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_a:
                 self.move_left = True
+                self.facing_right = False
             if event.key == pygame.K_d:
                 self.move_right = True
+                self.facing_right = True
             if event.key == pygame.K_SPACE:
                 if not self.is_jumping and self.can_jump:
                     self.is_jumping = True
@@ -65,7 +74,7 @@ class Player:
                 self.move_right = False
 
     def is_moving(self):
-        return self.move_left or self.move_right or self.is_jumping
+        return self.move_left or self.move_right or self.is_jumping or self.is_falling
 
     def calculate_new_position(self):
         new_x = self.x
@@ -76,11 +85,15 @@ class Player:
         if self.move_right:
             new_x += self.speed
 
-        if self.is_jumping:
+        if self.is_jumping or self.is_falling:
             new_y += self.vertical_velocity
             self.vertical_velocity += self.gravity
-            if self.vertical_velocity > 0:
+
+            if self.vertical_velocity > 1:
+                self.is_falling = True
                 self.is_jumping = False
+            else:
+                self.is_falling = False
 
         return new_x, new_y
 
